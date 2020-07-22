9182d4929ceb4555215df8560da00b7fac26f651
Samuel Amagbakhen <amagbakhensamuel@gmail.com>
2020-07-22 21:50:49 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,13 +4,3 @@ let htmlyear = document.querySelector("#year");
 
 htmlyear.textContent = year;
 
-
-/*preloader script*/
-
-let loader = document.querySelector(".loader");
-
-window.addEventListener("load", vanish);
-
-function vanish(){
-    loader.classList.add("disappear");
-}
