1bd25811eab6adfede7320854ecc34ac41d28aa5
Samuel Amagbakhen <amagbakhensamuel@gmail.com>
2020-07-28 14:06:59 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,32 @@ let htmlyear = document.querySelector("#year");
 
 htmlyear.textContent = year;
 
+var $animation_elements = $('.animation-test');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('rotate');
+    } else {
+      $element.removeClass('rotate');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+
+
